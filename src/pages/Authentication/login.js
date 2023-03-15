12d32b2d13e285f4714d03903c205c38ee82ef88
m-subhaan <muhammadsubhan221b@gmail.com>
2023-03-16 01:23:34 +0500

    login ui minor changes

--- src/pages/Authentication/login.js
+++ src/pages/Authentication/login.js
@@ -10,6 +10,7 @@ import {
   MDBCardTitle
 }
   from 'mdb-react-ui-kit';
+  import Image from '../../assets/image.png';
 
 function Login() {
   const [username, setUsername] = useState('')
@@ -23,28 +24,28 @@ function Login() {
     setPassword('')
   }, [clickSignIn])
   return (
-    <MDBContainer fluid className="p-3 my-5v" style={{ maxWidth: "70%", marginTop: "7%" }}>
+    <MDBContainer fluid className="p-3 my-5v" style={{ maxWidth: "70%", marginTop: "7%", border:"1px solid #808080"}}>
 
       <MDBRow>
 
         <MDBCol col='10' md='6'>
-          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image" />
+          <img src={Image} class="img-fluid" alt="Phone image" style={{borderRadius: "2%"}}/>
         </MDBCol>
 
         <MDBCol col='4' mt='6'>
-          <MDBCardTitle className="h1 fw-bold mb-5" >Welcome to YT Analytics</MDBCardTitle>
-          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" onChange={(e) => setUsername(e.target.value)} value={username} />
-          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" onChange={(e) => setPassword(e.target.value)} value={password} />
+        <p className="h1 fw-bold mb-5">Welcome to <span style={{color:'#00e778'}}>YT Analytics</span></p>
+          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" onChange={(e) => setUsername(e.target.value)} value={username} placeholder="jon.doe@example.com"/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="********"/>
 
-          <div className="d-flex justify-content-between mx-4 mb-4">
+          <div className="d-flex justify-content-between mb-4">
             <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-            <a href="#">Forgot password?</a>
+            <a style={{color:'#353738'}} href="#">Forgot password?</a>
           </div>
           <button type="button" class="btn btn-dark btn-lg mb-5" style={{ width: "100%" }} onClick={() => setClickSignIn(true)}>Sign in</button>
 
           {/* <MDBBtn className="mb-4 w-100" size="lg" onClick={setClickSignIn(true)}>Sign in</MDBBtn> */}
           {/* <MDBBtn className="mb-4 w-100" size="lg" onClick={(e)=>console.log('clicked')}>Sign in</MDBBtn> */}
-          <a href="signup" className="d-flex justify-content-between mx-4 mb-4" >Don't have an account? Register here</a>
+          <a href="signup" style={{color:'#353738'}} className="d-flex justify-content-between mx-4 mb-4" >Don't have an account? Register here</a>
         </MDBCol>
 
       </MDBRow>
