2449857c65a3845241144bcdfb2c7dfc7fb6c004
Muhammad Rafey <m.rafey659@gmail.com>
2023-03-15 23:32:47 +0500

    login aya rey

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import { Provider } from 'react-redux';
 
 import store from './redux/store';
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'))
--- src/pages/Authentication/login.js
+++ src/pages/Authentication/login.js
@@ -1,9 +1,56 @@
 import React, { useState, useEffect } from "react";
-
-const Login = () => {
-  console.log('Login')
-  return <>
-    Login Bitches!!!
-  </>
+import {
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
+  MDBIcon,
+  MDBInput,
+  MDBCheckbox,
+  MDBCardTitle
 }
-export default Login
+  from 'mdb-react-ui-kit';
+
+function Login() {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [clickSignIn, setClickSignIn] = useState(false)
+
+  useEffect(() => {
+    console.log(username, password)
+    setClickSignIn(false)
+    setUsername('')
+    setPassword('')
+  }, [clickSignIn])
+  return (
+    <MDBContainer fluid className="p-3 my-5v" style={{ maxWidth: "1600px", marginTop: "200px" }}>
+
+      <MDBRow>
+
+        <MDBCol col='10' md='6'>
+          <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg" class="img-fluid" alt="Phone image" />
+        </MDBCol>
+
+        <MDBCol col='4' mt='6'>
+          <MDBCardTitle className="h1 fw-bold mb-5" >Welcome to YT Analytics</MDBCardTitle>
+          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" onChange={(e) => setUsername(e.target.value)} value={username} />
+          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" onChange={(e) => setPassword(e.target.value)} value={password} />
+
+          <div className="d-flex justify-content-between mx-4 mb-4">
+            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+            <a href="!#">Forgot password?</a>
+          </div>
+          <button type="button" class="btn btn-primary btn-lg mb-5" style={{ width: "100%" }} onClick={() => setClickSignIn(true)}>Sign in</button>
+
+          {/* <MDBBtn className="mb-4 w-100" size="lg" onClick={setClickSignIn(true)}>Sign in</MDBBtn> */}
+          {/* <MDBBtn className="mb-4 w-100" size="lg" onClick={(e)=>console.log('clicked')}>Sign in</MDBBtn> */}
+          <a href="signup" className="d-flex justify-content-between mx-4 mb-4" >Don't have an account? Register here</a>
+        </MDBCol>
+
+      </MDBRow>
+
+    </MDBContainer>
+  );
+}
+
+export default Login;
