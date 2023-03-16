ad0d4375186fdffcd22d1479dc645f2e4eadde32
m-subhaan <muhammadsubhan221b@gmail.com>
2023-03-16 22:24:14 +0500

    pricing page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,14 +21,14 @@ function MenuBar() {
                         />{' '}
                         YT Analytics
                     </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse id="navbarScroll">
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto ml-5">
-                            <Nav.Link  href="/" style={{ color: 'white' }}>Home</Nav.Link>
-                            <Nav.Link  href="/pricing" style={{ color: 'white' }}>Pricing</Nav.Link>
+                            <Nav.Link href="/" style={{ color: 'white' }}>Home</Nav.Link>
+                            <Nav.Link href="/pricing" style={{ color: 'white' }}>Pricing</Nav.Link>
                         </Nav>
                         <Form className="d-flex">
-                            <Button href="/login" variant="" style={{ color: 'white' }} >Login</Button>
+                            <Button href="/login" variant="outline-success" className="ml-5" style={{ color: 'white' }} >Login</Button>
                             <Button href="/signup" variant="outline-success ml-2" style={{ color: 'white' }}>Signup</Button>
                         </Form>
                     </Navbar.Collapse>
--- src/pages/Pricing/pricing.js
+++ src/pages/Pricing/pricing.js
@@ -6,43 +6,43 @@ import {
     MDBCol,
 }
     from 'mdb-react-ui-kit';
+    import './styles.css';
 
 const Pricing = () => {
     return (
         <>
-            <MDBContainer className="my-5">
-                <MDBRow>
-                    <MDBCol md='6' className="mx-auto">
-                        <center>
-                            <p className="h1 fw-bold mb-2" style={{ color: '#00e778' }}>Pricing Details</p>
-                            <p className="h3 fw-bold mb-3">Start building today and save hours from the process</p>
-                            <p className="mb-3">Become part of our club and get access to complete features</p>
-                        </center>
-                    </MDBCol>
-                </MDBRow>
+        <MDBContainer className="my-5">
+          <MDBRow>
+            <MDBCol md='6' className="mx-auto">
+              <center>
+                <p className="h1 fw-bold mb-2" style={{ color: '#00e778' }}>Pricing Details</p>
+                <p className="h3 fw-bold mb-3">Start building today and save hours from the process</p>
+                <p className="mb-3">Become part of our club and get access to complete features</p>
+              </center>
+            </MDBCol>
+          </MDBRow>
+  
+          <MDBRow>
+            <MDBCol md='6' className="mx-auto">
+            <div className='outer-container'>
+              <div className="inner-container">
+                <h1>Choose a Plan</h1>
+                <div className="price-plan">
+                  <h2>Basic</h2>
+                  <h3>$9.99 / month</h3>
+                  <p>Analytics</p><hr></hr>
+                  <p>Analytics</p><hr></hr>
+                  <p>Analytics</p><hr></hr>
+                  <p>Analytics</p><hr></hr>
 
-                <MDBRow>
-                    <MDBCol md='6' className="mx-auto">
-                        <PricingTable highlightColor='#00e778'>
-                            <PricingSlot title='Basic' priceText='$10/month'>
-                                <PricingDetail> <b>15</b> projects</PricingDetail>
-                                <PricingDetail> <b>5 GB</b> storage</PricingDetail>
-                                <PricingDetail> <b>5</b> users</PricingDetail>
-                                <PricingDetail strikethrough> <b>Time tracking</b></PricingDetail>
-                                <button style={{
-                                    width: "50%",
-                                    display: 'block',
-                                    margin: '0 auto',
-
-                                }}
-                                    className="btn btn-lg btn-dark mt-5">Buy Now</button>
-                            </PricingSlot>
-                        </PricingTable>
-                    </MDBCol>
-                </MDBRow>
-
-            </MDBContainer>
-        </>
+                  <button>Select Plan</button>
+                </div>
+              </div>
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      </>
     );
 };
 
