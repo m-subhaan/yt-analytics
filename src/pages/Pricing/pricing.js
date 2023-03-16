620f420fc9f6ceed9b74833def52d99e92d3a382
m-subhaan <muhammadsubhan221b@gmail.com>
2023-03-16 23:32:11 +0500

    pricing edited

--- src/pages/Pricing/pricing.js
+++ src/pages/Pricing/pricing.js
@@ -1,39 +1,62 @@
 import React from "react";
-import { PricingTable, PricingSlot, PricingDetail } from 'react-pricing-table';
 import {
-    MDBContainer,
-    MDBRow,
-    MDBCol,
+  MDBContainer,
+  MDBRow,
+  MDBCol
 }
-    from 'mdb-react-ui-kit';
-    import './styles.css';
+  from 'mdb-react-ui-kit';
+import { FaCheck } from 'react-icons/fa';
+import './styles.css';
+
 
 const Pricing = () => {
-    return (
-        <>
-        <MDBContainer className="my-5">
-          <MDBRow>
-            <MDBCol md='6' className="mx-auto">
-              <center>
-                <p className="h1 fw-bold mb-2" style={{ color: '#00e778' }}>Pricing Details</p>
-                <p className="h3 fw-bold mb-3">Start building today and save hours from the process</p>
-                <p className="mb-3">Become part of our club and get access to complete features</p>
-              </center>
-            </MDBCol>
-          </MDBRow>
-  
-          <MDBRow>
-            <MDBCol md='6' className="mx-auto">
-            <div className='outer-container'>
+  return (
+    <>
+      <MDBContainer className="my-5">
+        <MDBRow>
+          <MDBCol md="6" className="mx-auto">
+            <center>
+              <p className="h1 fw-bold mb-2" style={{ color: "#00e778" }}>
+                Pricing Details
+              </p>
+              <p className="h3 fw-bold mb-3">
+                Start building today and save hours from the process
+              </p>
+              <p className="mb-3">
+                Become part of our club and get access to complete features
+              </p>
+            </center>
+          </MDBCol>
+        </MDBRow>
+
+        <MDBRow>
+          <MDBCol md="6" className="mx-auto">
+            <div className="outer-container">
               <div className="inner-container">
                 <h1>Choose a Plan</h1>
                 <div className="price-plan">
                   <h2>Basic</h2>
                   <h3>$9.99 / month</h3>
-                  <p>Analytics</p><hr></hr>
-                  <p>Analytics</p><hr></hr>
-                  <p>Analytics</p><hr></hr>
-                  <p>Analytics</p><hr></hr>
+                  <p>
+                    <FaCheck className="me-5" />
+                    Analytics
+                  </p>
+                  <hr />
+                  <p>
+                    <FaCheck className="me-5" />
+                    Analytics
+                  </p>
+                  <hr />
+                  <p>
+                    <FaCheck className="me-5" />
+                    Analytics
+                  </p>
+                  <hr />
+                  <p>
+                    <FaCheck className="me-5" />
+                    Analytics
+                  </p>
+                  <hr />
 
                   <button>Select Plan</button>
                 </div>
@@ -42,8 +65,9 @@ const Pricing = () => {
           </MDBCol>
         </MDBRow>
       </MDBContainer>
-      </>
-    );
+    </>
+  );
 };
 
 export default Pricing;
+
