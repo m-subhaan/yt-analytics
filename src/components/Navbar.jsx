e275a9de24965f60fb8b7f8b226a3a38bf5035fa
m-subhaan <muhammadsubhan221b@gmail.com>
2023-03-19 03:11:21 +0500

    page loading on links clicked removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import Navbar from 'react-bootstrap/Navbar';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+
 
 import BrandLogo from '../assets/BrandLogo.png';
 
@@ -11,25 +13,35 @@ function MenuBar() {
         <>
             <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
                 <Container>
-                    <Navbar.Brand href="/">
-                        <img
-                            alt=""
-                            src={BrandLogo}
-                            width="30"
-                            height="30"
-                            className="d-inline-block align-top"
-                        />{' '}
-                        YT Analytics
-                    </Navbar.Brand>
+                    <Link to="/" style={{ textDecoration: "none" }}>
+                        <Navbar.Brand href="/">
+                            <img
+                                alt=""
+                                src={BrandLogo}
+                                width="30"
+                                height="30"
+                                className="d-inline-block align-top"
+                            />{' '}
+                            YT Analytics
+                        </Navbar.Brand>
+                    </Link>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="me-auto ml-5">
-                            <Nav.Link href="/" style={{ color: 'white' }}>Home</Nav.Link>
-                            <Nav.Link href="/pricing" style={{ color: 'white' }}>Pricing</Nav.Link>
+                            <Link to="/" style={{ textDecoration: "none" }}>
+                                <Nav.Link href="/" style={{ color: 'white' }}>Home</Nav.Link>
+                            </Link>
+                            <Link to="/pricing" style={{ textDecoration: "none" }}>
+                                <Nav.Link href="/pricing" style={{ color: 'white' }}>Pricing</Nav.Link>
+                            </Link>
                         </Nav>
                         <Form className="d-flex">
-                            <Button href="/login" variant="outline-success" className="ml-5" style={{ color: 'white' }} >Login</Button>
-                            <Button href="/signup" variant="outline-success ml-2" style={{ color: 'white' }}>Signup</Button>
+                            <Link to="/login">
+                                <Button variant="outline-success" className="ml-5" style={{ color: 'white' }} >Login</Button>
+                            </Link>
+                            <Link to="/signup">
+                                <Button variant="outline-success ml-2" style={{ color: 'white' }}>Signup</Button>
+                            </Link>
                         </Form>
                     </Navbar.Collapse>
                 </Container>
--- src/pages/Authentication/login.js
+++ src/pages/Authentication/login.js
@@ -7,6 +7,8 @@ import {
   MDBCheckbox,
 }
   from 'mdb-react-ui-kit';
+  import  { Link } from 'react-router-dom';
+
   import MenuBar from "../../components/Navbar";
 
   import Image from '../../assets/image.png';
@@ -41,11 +43,11 @@ function Login() {
             <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
             <a style={{color:'#353738'}} href="/login">Forgot password?</a>
           </div>
-          <button type="button" class="btn btn-dark btn-lg mb-5" style={{ width: "100%" }} onClick={() => setClickSignIn(true)}>Sign in</button>
+          <Link to="/dashboard"><button type="button" class="btn btn-dark btn-lg mb-5" style={{ width: "100%" }} onClick={() => setClickSignIn(true)}>Sign in</button></Link>
 
           {/* <MDBBtn className="mb-4 w-100" size="lg" onClick={setClickSignIn(true)}>Sign in</MDBBtn> */}
           {/* <MDBBtn className="mb-4 w-100" size="lg" onClick={(e)=>console.log('clicked')}>Sign in</MDBBtn> */}
-          <a href="signup" style={{color:'#353738'}} className="d-flex justify-content-between mx-4 mb-4" >Don't have an account? Register here</a>
+          <Link to="/signup"><a href='/signup' style={{color:'#353738'}} className="d-flex justify-content-between mx-4 mb-4" >Don't have an account? Register here</a></Link>
         </MDBCol>
 
       </MDBRow>
