e1d7d465a6b62e0beaddd1246a5d41b9e71f1298
Muhammad Rafey <m.rafey659@gmail.com>
2023-03-19 03:56:46 +0500

    dashboard

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import SideBar from "../components/SideBar/Sidebar";
+import Chart from "../components/Chart.jsx";
 import "./App.css";
+import { Card, CardHeader, CardBody, CardTitle, Button, CardFooter, CardText, Col, Row } from "reactstrap"
 
 const Dashboard = () => {
   const [sidebarIsOpen, setSidebarOpen] = useState(true);
@@ -10,10 +12,45 @@ const Dashboard = () => {
 
 
 
-  return (<>
+  return (<div className="d-flex flex-row">
     <SideBar toggle={toggleSidebar} isOpen={sidebarIsOpen} />
+    <div className="d-flex flex-col">
+      <Row sm="12" className="m-5 h2">Dashboard</Row>
+      <Row className="d-flex m-5">
+        <Col sm="4">
+          <Card body>
+            <CardTitle tag="h5">
+              Perofrmance:
+            </CardTitle>
+            <div className="d-flex flex-row">
+              <CardText className="m-2">
+                26% income.
+              </CardText>
+              <div className = "m-1" style={{ borderLeft: '1px solid grey', height: '50px' }}></div>
+              <CardText className="m-2">
+                26% income.
+              </CardText>
+            </div>
+            <Button>
+             Check reasons
+            </Button>
+          </Card>
+        </Col>
+        <Col sm="8">
+          <Card body>
+            <CardTitle tag="h5">
+              Some stats in graph
+            </CardTitle>
+            <CardText>
+              With supporting text below as a natural lead-in to additional content.
+            </CardText>
+            <Chart/>
 
-  </>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  </div>
   );
 };
 
