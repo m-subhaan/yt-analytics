3475f8cc5b54b5ee41ea3c68f15dc693ab12b93c
Muhammad Rafey <m.rafey659@gmail.com>
2023-03-25 02:21:49 +0500

    login

--- src/pages/Authentication/login.js
+++ src/pages/Authentication/login.js
@@ -7,49 +7,53 @@ import {
   MDBCheckbox,
 }
   from 'mdb-react-ui-kit';
-  import  { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-  import MenuBar from "../../components/Navbar";
-
-  import Image from '../../assets/image.png';
+import MenuBar from "../../components/Navbar";
+import { login } from '../../services/BoilerService'
+import Image from '../../assets/image.png';
 
 function Login() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [clickSignIn, setClickSignIn] = useState(false)
 
-  useEffect(() => {
-    setClickSignIn(false)
+  const loginUser = async () => {
+    console.log('sending req....')
+    console.log(username, password)
+    const res = await login(username, password)
+    console.log(username, password)
+    console.log('*'.repeat(100))
+    console.log(res)
     setUsername('')
     setPassword('')
-  }, [clickSignIn])
+  }
   return (
     <>
-    <MenuBar/>
-    <MDBContainer fluid className="p-3 my-5" style={{ maxWidth: "70%", marginTop: "7%", border:"1px solid #808080"}}>
+      <MenuBar />
+      <MDBContainer fluid className="p-3 my-5" style={{ maxWidth: "70%", marginTop: "7%", border: "1px solid #808080" }}>
 
-      <MDBRow>
+        <MDBRow>
 
-        <MDBCol col='10' md='6'>
-          <img src={Image} class="img-fluid" alt="login" style={{borderRadius: "2%"}}/>
-        </MDBCol>
+          <MDBCol col='10' md='6'>
+            <img src={Image} class="img-fluid" alt="login" style={{ borderRadius: "2%" }} />
+          </MDBCol>
 
-        <MDBCol col='4' mt='6'>
-        <p className="h1 fw-bold mb-5">Welcome to <span style={{color:'#00e778'}}>YT Analytics</span></p>
-          <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" onChange={(e) => setUsername(e.target.value)} value={username} placeholder="jon.doe@example.com"/>
-          <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="********"/>
+          <MDBCol col='4' mt='6'>
+            <p className="h1 fw-bold mb-5">Welcome to <span style={{ color: '#00e778' }}>YT Analytics</span></p>
+            <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" onChange={(e) => setUsername(e.target.value)} value={username} placeholder="jon.doe@example.com" />
+            <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" onChange={(e) => setPassword(e.target.value)} value={password} placeholder="********" />
 
-          <div className="d-flex justify-content-between mb-4">
-            <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-            <a style={{color:'#353738'}} href="/login">Forgot password?</a>
-          </div>
-          <Link to="/analytics"><button type="button" class="btn btn-dark btn-lg mb-5" style={{ width: "100%" }} onClick={() => setClickSignIn(true)}>Sign in</button></Link>
-          <Link to="/signup"><a href='/signup' style={{color:'#353738'}} className="d-flex justify-content-between mx-4 mb-4" >Don't have an account? Register here</a></Link>
-        </MDBCol>
+            <div className="d-flex justify-content-between mb-4">
+              <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
+              <a style={{ color: '#353738' }} href="/login">Forgot password?</a>
+            </div>
+            <Link to="/analytics"><button type="button" class="btn btn-dark btn-lg mb-5" style={{ width: "100%" }} onClick={() => loginUser()}>Sign in</button></Link>
+            <Link to="/signup"><a href='/signup' style={{ color: '#353738' }} className="d-flex justify-content-between mx-4 mb-4" >Don't have an account? Register here</a></Link>
+          </MDBCol>
 
-      </MDBRow>
+        </MDBRow>
 
-    </MDBContainer>
+      </MDBContainer>
     </>
   );
 }
--- src/services/BoilerService.js
+++ src/services/BoilerService.js
@@ -1,21 +1,9 @@
 import axios from 'axios';
 import { BASE_URL } from "../constants";
-export async function login() {
-    //lay boss
-    //GET /api/users/login/{email}/{password}
-
-    try {
-        return await axios(`${BASE_URL}/api/users`);
-    } catch (error) {
-        return error;
-    }
-
-}
+export const login = async (user, pass) => axios.get(`${BASE_URL}/users/login/${user}/${pass}`);
 
 export async function createUser(data) {
-    console.log(data)
     const response = await axios.post(`${BASE_URL}/users/register`, data)
     if (response.status == 200) return true;
     return response.body?.error || 'Server error'
-
 }
